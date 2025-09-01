714b9d57c1da48cec026f2d42b3e0dea40da317d
piotrhm <helm.piotr@gmail.com>
2025-09-01 22:09:19 +0200

    fix - part 7

--- utils_xs_lora/initialization_utils.py
+++ utils_xs_lora/initialization_utils.py
@@ -104,9 +104,9 @@ def find_and_initialize(model, peft_config, adapter_name, reconstr_type, reconst
                     target.lora.forward = types.MethodType(forward_latent, target.lora)
                     
                     replace_module_weights(target.lora.A, replacement_encoder_weight.T)
-                    target.default_lora_latent_mapping = torch.nn.Linear(lora_config.r, lora_config.r, bias=False)
-                    init_module_weights(target.default_lora_latent_mapping, sigma=0.00001)
-                    target.default_lora_latent_mapping.to(target.lora.A.device)
+                    target.lora.default_lora_latent_mapping = torch.nn.Linear(lora_config.r, lora_config.r, bias=False)
+                    init_module_weights(target.lora.default_lora_latent_mapping, sigma=0.00001)
+                    target.lora.default_lora_latent_mapping.to(target.lora.A.device)
 
                     target.lora.A.requires_grad = False  # only the r*r matrix will be tuned
                     target.lora.B.requires_grad = False  # only the r*r matrix will be tuned
