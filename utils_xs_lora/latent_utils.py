6ef49d4d306cf36c8b6b5e18841b375f08c9fb6a
piotrhm <helm.piotr@gmail.com>
2025-09-08 17:21:59 +0200

    fix - part 47

--- utils_xs_lora/latent_utils.py
+++ utils_xs_lora/latent_utils.py
@@ -7,10 +7,5 @@ def forward_latent(self, x: torch.Tensor):
     B = self.B.to(x.device)
     L = self.default_lora_latent_mapping.to(x.device)
     
-    print("Inside forward_latent")
-    print(x @ A)
-    print(x @ A @ L.weight)
-    print(x @ A @ L.weight @ B)
-    
     delta = x @ A @ L.weight @ B
     return delta * self.alpha
