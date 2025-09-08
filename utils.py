ef40613e5d85661e8baebf44a9b7665e4db09312
piotrhm <helm.piotr@gmail.com>
2025-09-08 20:24:29 +0200

    fix - part 58

--- utils.py
+++ utils.py
@@ -63,10 +63,9 @@ def apply_lora_to_model(model, lora_state_dict, alpha=4):
         alpha (float): Scaling factor for the LoRA update (default: 4).
     """
     model_sd = model.state_dict()
-    print(lora_state_dict)
         
     print("Applying LoRA adapters to model weights...")
-    for lora_L_key in [k for k in lora_state_dict if k.endswith(".lora.default_lora_latent_mapping")]:
+    for lora_L_key in [k for k in lora_state_dict if k.endswith(".lora.default_lora_latent_mapping.weight")]:
         prefix = lora_L_key[:-len(".lora.default_lora_latent_mapping")]
         print(prefix)
         
