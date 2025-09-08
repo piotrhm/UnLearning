7c1224e041715193a14ac4581cd8e44bea6b8749
piotrhm <helm.piotr@gmail.com>
2025-09-08 14:11:41 +0200

    fix - part 34

--- utils_xs_lora/svd_utils.py
+++ utils_xs_lora/svd_utils.py
@@ -46,6 +46,8 @@ def svd_lowrank(W: torch.Tensor, rank: int, sigma_split: str = "sym", eps: float
     U = U[:, :r]            # (m, r)
     S = S[:r]               # (r,)
     Vh = Vh[:r, :]          # (r, n)
+    
+    print(U, S, Vh)
 
     # Avoid sqrt(0) and tiny underflow in half/bfloat16 later.
     S_safe = torch.clamp(S, min=eps)
